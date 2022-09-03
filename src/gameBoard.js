79d4f01bf7ffee6b668ea2c8a6a30f55202b1e21
DrantDumani <darnjames@gmail.com>
2022-09-02 20:57:09 -0400

    Completed all functions and tests for gameboard

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -6,20 +6,44 @@ function createGameBoard() {
     }
   }
 
+  const oneDimIndex = (y, x) => y * 10 + x;
+
   const placeShip = (coords, fn, len = 4) => {
     const [y, x] = coords;
     if (x > 9 || y > 9) {
       return "Index out of bounds";
     }
-    const index = y * 10 + x;
+    const index = oneDimIndex(y, x);
     gameBoardShips.push(fn(index, len));
   };
 
   const gameBoardShips = [];
-
   const getShips = () => gameBoardShips;
 
-  return { placeShip, getShips };
+  const attackedTiles = [];
+  const getAttackedTiles = () => attackedTiles;
+
+  const receiveAttack = (coord) => {
+    const [y, x] = coord;
+    const index = oneDimIndex(y, x);
+    if (attackedTiles.includes(index)) {
+      return;
+    }
+    attackedTiles.push(index);
+    for (const ship of gameBoardShips) {
+      if (ship.hit(index)) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const allShipsSunk = () => {
+    const shipStatuses = gameBoardShips.map((ship) => ship.isSunk());
+    return shipStatuses.every((status) => status === true);
+  };
+
+  return { placeShip, getShips, receiveAttack, allShipsSunk, getAttackedTiles };
 }
 
 export default createGameBoard;
--- src/tests/gameBoard.test.js
+++ src/tests/gameBoard.test.js
@@ -12,3 +12,38 @@ test("Gameboard receives invalid index. Fails to place ship", () => {
   testBoard.placeShip([11, 4], createShip, 4);
   expect(testBoard.getShips().length).toBe(0);
 });
+
+test("Gameboard receives attack coords and hits ship.", () => {
+  const testBoard = createGameBoard();
+  testBoard.placeShip([9, 4], createShip, 4);
+  expect(testBoard.receiveAttack([9, 6])).toBe(true);
+});
+
+test("Gameboard receives attack coords and misses every ship.", () => {
+  const testBoard = createGameBoard();
+  testBoard.placeShip([9, 4], createShip, 4);
+  expect(testBoard.receiveAttack([9, 8])).toBe(false);
+});
+
+test("Gameboard checks to make sure all ships have been sunk", () => {
+  const testBoard = createGameBoard();
+  testBoard.placeShip([9, 4], createShip, 1);
+  testBoard.receiveAttack([9, 4]);
+  expect(testBoard.allShipsSunk()).toBe(true);
+});
+
+test("Gameboard checks to make sure all ships have been sunk", () => {
+  const testBoard = createGameBoard();
+  testBoard.placeShip([9, 4], createShip, 1);
+  testBoard.placeShip([5, 4], createShip, 4);
+  testBoard.receiveAttack([9, 4]);
+  expect(testBoard.allShipsSunk()).toBe(false);
+});
+
+test("Make sure that Gameboard is recording previously attacked tiles", () => {
+  const testBoard = createGameBoard();
+  testBoard.receiveAttack([8, 2]);
+  testBoard.receiveAttack([9, 6]);
+  testBoard.receiveAttack([0, 7]);
+  expect(testBoard.getAttackedTiles()).toEqual([82, 96, 7]);
+});
