eb3632699f5b38d34ee85b753d9feae918cab92a
DrantDumani <darnjames@gmail.com>
2022-09-04 00:08:09 -0400

    Added test and functions for CPU player

--- src/player.js
+++ src/player.js
@@ -1,3 +1,21 @@
-function createPlayer() {}
+function createPlayer(str) {
+  const type = str;
+  if (str === "CPU") {
+    const availableMoves = [];
+    for (let i = 0; i < 100; i++) {
+      availableMoves.push(i);
+    }
+
+    const randomAttack = () => {
+      const index = Math.floor(Math.random() * availableMoves.length);
+      const move = availableMoves[index];
+      availableMoves.splice(index, 1);
+      return move;
+    };
+
+    return { randomAttack, type };
+  }
+  return { type };
+}
 
 export default createPlayer;
