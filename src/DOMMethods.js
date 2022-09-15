ed857d6ad12fa491591685c14dc304fae7c97755
DrantDumani <darnjames@gmail.com>
2022-09-14 23:04:35 -0400

    Completed v1 of tests for DOM methods

---
+++ src/DOMMethods.js
@@ -0,0 +1,23 @@
+function renderGameBoard(gameLoop, container, type) {
+  const plyrBoard = gameLoop.getAllBoardInfo()[0];
+
+  for (let i = 0; i < 100; i++) {
+    const btn = document.createElement("button");
+    if (plyrBoard[i] === undefined) {
+      btn.classList.add("blank-tile");
+    } else if (type === "activePlayer") {
+      btn.classList.add("ship-segment");
+    }
+    btn.dataset.index = i;
+    container.append(btn);
+  }
+}
+
+function handleClick(elem, gameLoop) {
+  const defendingBoard = gameLoop.getDefendingBoard();
+  const index = Number(elem.dataset.index);
+  defendingBoard.receiveAttack(index);
+  elem.disabled = true;
+}
+
+export { renderGameBoard, handleClick };
--- src/tests/DOMMethods.test.js
+++ src/tests/DOMMethods.test.js
@@ -2,7 +2,99 @@
  * @jest-environment jsdom
  */
 
-test("User can make attacks by clicking one of the buttons", () => {
-  // simulate button click and check to make sure a function gets called.
-  // On second thought, maybe write code for gameloop after all.
+import createGameBoard from "../gameBoard";
+import initGameLoop from "../gameLoop";
+import createPlayer from "../player";
+import createShip from "../ships";
+import { renderGameBoard, handleClick } from "../DOMMethods";
+
+function createDummyDOM() {
+  const container = document.createElement("div");
+  container.innerHTML = `
+      <div class="player1-board">
+        <div class="board-grid">
+      </div>
+      <div class="player2-board">
+        <div class="board-grid">
+      </div>
+    `;
+  return container;
+}
+
+test("DOM method populates gameboard element with 100 buttons", () => {
+  const testContainer = createDummyDOM();
+  const pBoard = testContainer.querySelector(".player1-board > .board-grid");
+  expect(pBoard.querySelectorAll("button").length).toBe(0);
+  const testLoop = initGameLoop(createGameBoard, createShip, createPlayer);
+  renderGameBoard(testLoop, pBoard);
+  expect(pBoard.querySelectorAll("button").length).toBe(100);
 });
+
+test("Active players' boards' buttons are assigned classes based on ship placement.", () => {
+  const testContainer = createDummyDOM();
+  const pBoard = testContainer.querySelector(".player1-board > .board-grid");
+  const testLoop = initGameLoop(createGameBoard, createShip, createPlayer);
+  renderGameBoard(testLoop, pBoard, "activePlayer");
+  const plyrBoard = testLoop.getAllBoardInfo()[0];
+  const boardTiles = pBoard.querySelectorAll("button");
+  for (let i = 0; i < boardTiles.length; i++) {
+    if (plyrBoard[i] === undefined) {
+      expect(boardTiles[i].classList.contains("blank-tile")).toBe(true);
+    } else if (typeof plyrBoard[i] === "object") {
+      expect(boardTiles[i].classList.contains("ship-segment")).toBe(true);
+    }
+  }
+});
+
+test("Clicking a tile on the defending board should make an attack on that tile", () => {
+  const testContainer = createDummyDOM();
+  const defendingDOMBoard = testContainer.querySelector(
+    ".player2-board > .board-grid"
+  );
+
+  const testLoop = initGameLoop(createGameBoard, createShip, createPlayer);
+  renderGameBoard(testLoop, defendingDOMBoard);
+  const gameButtons = defendingDOMBoard.querySelectorAll("button");
+  gameButtons.forEach((btn) =>
+    btn.addEventListener("click", (e) => {
+      handleClick(e.target, testLoop);
+    })
+  );
+
+  const defendingGameBoard = testLoop.getDefendingBoard();
+  const testIndex = Math.floor(Math.random() * gameButtons.length);
+  const attackSpy = jest.spyOn(defendingGameBoard, "receiveAttack");
+  const defendingTileZero =
+    defendingDOMBoard.querySelectorAll("button")[testIndex];
+  defendingTileZero.click();
+  expect(attackSpy).toBeCalledWith(testIndex);
+  const attackedTiles = defendingGameBoard.getAttackedIndices();
+  const enemyBoardArr = defendingGameBoard.getBoardArr();
+  if (enemyBoardArr[testIndex] === undefined) {
+    expect(attackedTiles[testIndex]).toBe(false);
+  } else if (typeof enemyBoardArr[testIndex] === "object") {
+    expect(attackedTiles[testIndex]).toBe(true);
+  }
+});
+
+test("Buttons are disabled after they're clicked", () => {
+  const testContainer = createDummyDOM();
+  const defendingDOMBoard = testContainer.querySelector(
+    ".player2-board > .board-grid"
+  );
+  const testLoop = initGameLoop(createGameBoard, createShip, createPlayer);
+  renderGameBoard(testLoop, defendingDOMBoard);
+  const gameButtons = defendingDOMBoard.querySelectorAll("button");
+  gameButtons.forEach((btn) =>
+    btn.addEventListener("click", (e) => {
+      handleClick(e.target, testLoop);
+    })
+  );
+  const testIndex = Math.floor(Math.random() * gameButtons.length);
+  const randomBtn = gameButtons[testIndex];
+  expect(randomBtn.disabled).toBe(false);
+  randomBtn.click();
+  expect(randomBtn.disabled).toBe(true);
+});
+
+test("");
